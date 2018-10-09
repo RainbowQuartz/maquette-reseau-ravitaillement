b70be6238bab4d777b2749fcbed2e4edaee27d62
Léona Chevrel <chevrel.leona@gmail.com>
2018-10-09 11:36:10 +0200

    added the mentions_legales_path to the routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'infos', to: 'static_pages#map'
   get 'photos', to: 'static_pages#photos'
   get 'contact', to: 'static_pages#contact'
+  get 'mentions_legales', to: 'static_pages#mentions_legales'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
